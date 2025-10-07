51bca2bad76c17004c177e9b86dac3d4f507868b
JRamses11 <ramchu_aldama@hotmail.com>
2025-10-07 11:48:12 -0700

    Ngfor NgIf

--- src/app/pages/dragonball/dragonball.ts
+++ src/app/pages/dragonball/dragonball.ts
@@ -1,9 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, computed, signal } from "@angular/core";
+// import { NgClass } from "@angular/common";
+interface Personaje {
+    id: number;
+    nombre: string;
+    poder: number;
+}
 
 @Component({
-    templateUrl: './dragonball.html'
+    templateUrl: './dragonball.html',
+    // imports: [NgClass]
 })
 
 
 export class dragonballPageComponent {
+
+    personajes = signal<Personaje[]>([
+        { id: 1, nombre: 'Goku', poder: 9001 },
+        { id: 2, nombre: 'Vegeta', poder: 8000 },
+        { id: 3, nombre: 'Krillin', poder: 3000 },
+        { id: 4, nombre: 'Piccolo', poder: 200 },
+    ]);
+
+    // clasePoder = computed(() => {
+    //     return {
+    //         'text-danger': true,
+    //     }
+    // })
 }
+
+
